f839d4e80990fb7da7a97922fd77328cab70e0c4
mdeboute <martin.deboute@gmail.com>
2021-04-08 20:15:52 +0200

    feat(app.src.main.java.hotelproject.controllers): create the first classes of the DB controllers

--- app/src/main/java/hotelproject/HotelProject.java
+++ app/src/main/java/hotelproject/HotelProject.java
@@ -4,33 +4,63 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import hotelproject.controllers.User;
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
 
 public class HotelProject extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        VBox root = new VBox();
-        root.setPadding(new Insets(5));
-        Label title = new Label("Welcome");
-        Label mysql;
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.4));
+        pane.setHgap(5.5);
+        pane.setVgap(5.5);
 
-//        try {
-//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/StarWars?user=tobias&password=abcd1234&useSSL=false");
-//            mysql = new Label("Driver found and connected");
-//
-//        } catch (SQLException e) {
-//            mysql = new Label("An error has occurred: " + e.getMessage());
-//        }
+        pane.add(new Label("Username :"), 0, 0);
+        final TextField username = new TextField();
+        pane.add(username, 1, 0);
+        pane.add(new Label("Password :"), 0, 1);
+        final PasswordField password = new PasswordField();
+        pane.add(password, 1, 1);
+        final Label result = new Label();
+        pane.add(result, 0, 2);
+        Button testLogin = new Button("Login");
+        pane.add(testLogin, 1, 2);
+        GridPane.setHalignment(testLogin, HPos.RIGHT);
 
-        root.getChildren().addAll(title);
 
-        primaryStage.setScene(new Scene(root, 400, 200));
-        primaryStage.setTitle("Hotel Manager");
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/StarWars?user=tobias&password=abcd1234&useSSL=false");
+            System.out.println("Driver found and connected");
+        } catch (SQLException e) {
+            System.out.println("\n An error has occurred" + e.getMessage());
+        }
+
+        //test
+        User admin = new User("mdeboute", "qwerty", true);
+
+        testLogin.setOnAction(e -> {
+            if (username.getText().equals(admin.getUsername()) && password.getText().equals(admin.getPassword())) {
+                result.setText("Success !");
+            }else{
+                result.setText("Fail !");
+            }
+        });
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Login");
         primaryStage.show();
     }
 }
