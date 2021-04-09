43fa3c3c69ad713c86ebe56f0a7cc9caec2b38b8
mdeboute <martin.deboute@gmail.com>
2021-04-09 14:48:27 +0200

    refactor(app.src.main.java.hotelproject.controllers): modify somes classes

--- app/src/main/java/hotelproject/controllers/DatabaseConnector.java
+++ app/src/main/java/hotelproject/controllers/DatabaseConnector.java
@@ -7,29 +7,25 @@ import java.util.Properties;
 
 
 public class DatabaseConnector {
-    private Connection conn = null;
+    private static Connection conn = null;
 
     public DatabaseConnector() {
     }
 
-    public static DatabaseConnector getInstance() {
-        return new DatabaseConnector();
-    }
-
-    public Connection getConnection() {
+    public static Connection getConnection() {
         return conn;
     }
 
-    public Connection createConnection(String url, String user, String password, String database) throws SQLException {
+    public static Connection createConnection(String url, String user, String password, String database) throws SQLException {
         if (conn != null) conn.close();
         try {
             Properties connectionProps = new Properties();
             connectionProps.put("user", user);
             connectionProps.put("password", password);
             connectionProps.put("database", database);
-            conn = DriverManager.getConnection(url + database, connectionProps);
+            conn = DriverManager.getConnection(url, connectionProps);
         } catch(SQLException e) {
-            System.out.println("Error : " + e);
+            System.err.println("Error : " + e);
             return null;
         }
         return conn;
