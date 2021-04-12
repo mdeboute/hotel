4702d8b6a2c2c6b5bb3eb8c78d908a3040cb7e93
Cloe Magnien <cloe.magnien@etu.u-bordeaux.fr>
2021-04-12 19:11:28 +0200

    user connection

--- app/src/main/java/hotelproject/HotelProject.java
+++ app/src/main/java/hotelproject/HotelProject.java
@@ -2,6 +2,7 @@ package hotelproject;
 
 import hotelproject.controllers.User;
 import hotelproject.views.Login;
+import hotelproject.views.MainPage;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -11,6 +12,8 @@ import javafx.stage.Stage;
 
 
 public class HotelProject extends Application {
+
+    User connectedUser = new User();
 
     /**
      * @param args the command line arguments
@@ -23,27 +26,50 @@ public class HotelProject extends Application {
     public void start(Stage primaryStage) {
 
         User user = new User("toto", "qwerty", true);
+
         Login login = new Login(user);
+        boolean isConnected = false;
 
         /**primaryStage.setScene(welcomeSceneBeforeAuth());
         primaryStage.setTitle("Hotel Manager");
         primaryStage.show();**/
 
+        login.getTestLoginButton().setOnAction(e -> { //test if the user exist in the database and has correct password
+            if (login.getUsername().getText().equals(user.getUsername()) && login.getPassword().getText().equals(user.getPassword())) {
+                login.getResult().setText("Success !");
+                connectedUser.setUsername(login.getUsername().getText());
+                connectedUser.setPassword(login.getPassword().getText());
+                //is user admin ?
+                //isConnected = login.isUserConnected();
+                primaryStage.close();
+                afterAuth(primaryStage);
+            }else{
+                login.getResult().setText("Fail !");
+            }
+        });
+
         primaryStage.setScene(login.getScene());
         primaryStage.setTitle("Login");
+        //primaryStage.setOnCloseRequest(e -> );
         primaryStage.show();
     }
 
-    private Scene welcomeSceneBeforeAuth() {
+    /**private Scene welcomeSceneBeforeAuth() {
         Button quit = new Button("Quit");
         Label welcome = new Label("Welcome to your Hotel Manager");
 
         Group group = new Group(welcome, quit);
 
         return new Scene(group,500, 300);
-    }
+    }**/
 
-    private Scene welcomeSceneAfterAuth() {
-        return new Scene(new Label(""), 500, 300);
+    private void afterAuth(Stage primaryStage) {
+        MainPage mainPage = new MainPage(connectedUser);
+
+        //button handling here
+
+        primaryStage.setScene(mainPage.getScene());
+        primaryStage.setTitle("Hotel Manager");
+        primaryStage.show();
     }
 }
---
+++ app/src/main/java/hotelproject/views/MainPage.java
@@ -0,0 +1,89 @@
+package hotelproject.views;
+
+import hotelproject.controllers.User;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class MainPage {
+    private User user;
+    private Scene scene;
+
+    //buttons
+    Button myPage;
+    Button logout;
+    Button addBooking;
+    Button viewBookings;
+    Button update;
+    Button updateUser;
+
+    public MainPage(User user) {
+        this.user = user;
+        createScene();
+    }
+
+    private void createScene() {
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.4));
+        pane.setHgap(5.5);
+        pane.setVgap(5.5);
+
+        String userStatus = "Reception staff";
+        myPage = new Button("My Page");
+        logout = new Button("Logout");
+        addBooking = new Button("Add a booking");
+        viewBookings = new Button("Vieaw bookings");
+        update = new Button("Update bookings");
+
+        if (user.isAdmin()) {
+            userStatus = "Administrator";
+            updateUser = new Button("Update an user");
+        }
+
+        pane.add(myPage, 1, 0);
+        pane.add(logout, 2, 0);
+        Label status = new Label("Your status is : " + userStatus);
+        pane.add(status, 0, 1);
+        pane.add(addBooking, 0, 2);
+        pane.add(viewBookings, 0, 3);
+        pane.add(update, 0, 4);
+        pane.add(updateUser, 0, 5);
+
+        scene = new Scene(pane);
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public Button getMyPageButton() {
+        return myPage;
+    }
+
+    public Button getLogoutButton() {
+        return logout;
+    }
+
+    public Button getAddBookingButton() {
+        return addBooking;
+    }
+
+    public Button getViewBookingsButton() {
+        return viewBookings;
+    }
+
+    public Button getUpdateButton() {
+        return update;
+    }
+
+    public Button getUpdateUserButton() {
+        return updateUser;
+    }
+}
