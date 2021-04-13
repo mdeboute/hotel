4bda68bfcee858c3dfced96035ab85d19abfd305
Oscar Johansson <ojoco09@student.lnu.se>
2021-04-13 11:29:38 +0200

    test(src.test.java.hotelproject.readers):add roomsreadertest method
    add roomsreadertest method testreadroomsfromcsv

--- app/src/test/java/hotelproject/readers/RoomsReaderTest.java
+++ app/src/test/java/hotelproject/readers/RoomsReaderTest.java
@@ -4,9 +4,17 @@ import static org.junit.Assert.*;
 
 import hotelproject.controllers.Room;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import org.junit.Test;
 
 public class RoomsReaderTest {
+
+  @Test
+  public void testReadRoomsFromCSV(){
+    String filepath = "app/src/main/java/hotelproject/instances/room.csv";
+    ArrayList<Room> rooms = RoomsReader.readRoomsFromCSV(filepath);
+    assertTrue("Should equal true",rooms instanceof ArrayList);
+  }
 
   @Test
   public void testCreateRoom() throws Exception {
