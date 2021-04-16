1b9757f6eae4e71fa6ed3d3feec76ee5ce24fd22
Oscar Johansson <ojoco09@student.lnu.se>
2021-04-16 14:37:22 +0200

    test(src.test.java.hotelproject.controllers.db): added test to roomsdbtest
    added tests to roomsdbtest

--- app/src/test/java/hotelproject/controllers/db/RoomsDBTest.java
+++ app/src/test/java/hotelproject/controllers/db/RoomsDBTest.java
@@ -12,13 +12,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-public class RoomsDBTest<Arraylist> {
+public class RoomsDBTest {
 
 
   private DatabaseManager dbm;
   private ArrayList<String> log;
-  private Room testRoom;
-  private RoomType testRoomType;
+  private Room testRoom1;
+  private RoomType testRoomType1;
   private String t_name;
   private int r_num;
 
@@ -28,7 +28,8 @@ public class RoomsDBTest<Arraylist> {
     log = new ArrayList<>();
     r_num = 666;
     t_name = generateRandomString();
-    testRoomType = new RoomType(t_name, 1, 1, 1, 1, 1, 1, 1, 1);
+    testRoom1 = new Room(r_num,6,"Single", 1);
+    testRoomType1 = new RoomType(t_name, 1, 1, 1, 1, 1, 1, 1, 1);
   }
 
 
@@ -41,12 +42,14 @@ public class RoomsDBTest<Arraylist> {
   @Test
   public void testAddRoomType() {
 
-    dbm.rdb.addRoomType(testRoomType);
+    // Ensure first that the database does not contain testRoomType
+    dbm.rdb.deleteRoomType(testRoomType1);
+    dbm.rdb.addRoomType(testRoomType1);
     boolean successfulUpdate = false;
 
     List<RoomType> roomTypes = dbm.rdb.findAllRoomTypes();
     for (RoomType roomType : roomTypes) {
-      if (testRoomType.equals(roomType)) {
+      if (testRoomType1.getT_name().equals(roomType.getT_name())) {
         successfulUpdate = true;
         break;
       }
@@ -54,16 +57,32 @@ public class RoomsDBTest<Arraylist> {
     assertTrue(successfulUpdate);
   }
 
+  @Test
+  public void testDeleteRoomType() {
+    dbm.rdb.addRoomType(testRoomType1);
+    dbm.rdb.deleteRoomType(testRoomType1);
+    boolean successfulUpdate = true; // Note: 'true'
 
+    List<RoomType> roomTypes = dbm.rdb.findAllRoomTypes();
+    for (RoomType roomType : roomTypes) {
+      if (roomType.getT_name().equals(testRoomType1.getT_name())) {
+        successfulUpdate = false;
+        break;
+      }
+    }
+    assertTrue(successfulUpdate);
+  }
 
   @Test
   public void testAddRoom() {
-    dbm.rdb.addRoom(testRoom);
+    // Have to ensure that testRoom does not already exist in the database
+    dbm.rdb.deleteRoom(testRoom1);
+    dbm.rdb.addRoom(testRoom1);
     boolean successfulUpdate = false;
 
     List<Room> rooms = dbm.rdb.findAllRooms();
     for (Room room : rooms) {
-      if (room.equals(testRoom)) {
+      if (room.getR_num() == testRoom1.getR_num()) {
         successfulUpdate = true;
         break;
       }
@@ -72,6 +91,66 @@ public class RoomsDBTest<Arraylist> {
   }
 
   @Test
-  public void testReadRooms() {
+  public void testDeleteRoom() {
+    // Have to ensure that testRoom does exist in the database; hence the reverse order compared to previous test
+    dbm.rdb.deleteRoom(testRoom1);
+    dbm.rdb.addRoom(testRoom1);
+    dbm.rdb.deleteRoom(testRoom1);
+    boolean successfulUpdate = true;
+
+    List<Room> rooms = dbm.rdb.findAllRooms();
+    for (Room room : rooms) {
+      if (room.getR_num() == testRoom1.getR_num()) {
+        successfulUpdate = false;
+        break;
+      }
+    }
+    assertTrue(successfulUpdate);
+  }
+
+  @Test
+  public void testUpdateRoomType() {
+    dbm.rdb.deleteRoomType(testRoomType1);
+    dbm.rdb.addRoomType(testRoomType1);
+
+    // Update testRoomType1, from has bed (1) to does not have bed (0)
+    testRoomType1.setBeds(0);
+
+    // test
+    dbm.rdb.updateRoomType(testRoomType1, testRoomType1.getT_name());
+
+    boolean successfulUpdate = false;
+
+    List<RoomType> roomTypes = dbm.rdb.findAllRoomTypes();
+    for (RoomType roomType : roomTypes) {
+      if (testRoomType1.getT_name().equals(roomType.getT_name())) {
+        successfulUpdate = true;
+        break;
+      }
+    }
+    assertTrue(successfulUpdate);
+  }
+
+  @Test
+  public void testUpdateRoom() {
+    dbm.rdb.deleteRoom(testRoom1);
+    dbm.rdb.addRoom(testRoom1);
+
+    // Update testRoom1, from has r_type "Single" r_type "Double"
+    testRoom1.setR_type("Double");
+
+    // test
+    dbm.rdb.updateRoom(testRoom1, testRoom1.getR_num());
+
+    boolean successfulUpdate = false;
+
+    List<Room> rooms = dbm.rdb.findAllRooms();
+    for (Room room : rooms) {
+      if (testRoom1.getR_num() == room.getR_num()) {
+        successfulUpdate = true;
+        break;
+      }
+    }
+    assertTrue(successfulUpdate);
   }
 }
