68e6f1b46236ad957cfb861d88c6e48b5e643320
Cloe Magnien <cloe.magnien@etu.u-bordeaux.fr>
2021-04-16 16:40:13 +0200

    refactor(views): new abstract class View, implementation not finished yet

---
+++ app/src/main/java/hotelproject/views/View.java
@@ -0,0 +1,37 @@
+package hotelproject.views;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+public abstract class  View {
+    Scene scene;
+
+    public View() {
+        createScene();
+    }
+    abstract void createScene();
+
+    VBox createHeader(String title, String subtitle) {
+        Label titleL = new Label(title);
+        titleL.setStyle("-fx-font-weight: bold;");
+        titleL.setFont(Font.font(18));
+
+        Label subtitleL = new Label(subtitle);
+        subtitleL.setFont(Font.font(14));
+
+        VBox header = new VBox(titleL, subtitleL);
+        header.setAlignment(Pos.CENTER);
+        header.setPadding(new Insets(10.0, 10.0, 2.0, 10.0));
+        header.setSpacing(5.5);
+
+        return header;
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+}
