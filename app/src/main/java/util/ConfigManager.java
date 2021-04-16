7c8743f6462d193149a95408c8d3bafe81f86ad9
DrMagazi <atakan.osman.coban@gmail.com>
2021-04-16 15:42:53 +0300

    feat(util): new class, config manager

---
+++ app/src/main/java/util/ConfigManager.java
@@ -0,0 +1,63 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class ConfigManager {
+    private String configPath;
+
+    public ConfigManager(String configPath) {
+        this.configPath = configPath;
+    }
+
+    public void setConfigPath(String configPath) {
+        this.configPath = configPath;
+    }
+
+    public String getConfigPath() {
+        return configPath;
+    }
+
+    public String getPValue(String key) {
+        Properties prop = new Properties();
+        InputStream is = null;
+        try {
+            is = new FileInputStream(configPath);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            prop.load(is);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return prop.getProperty(key);
+    }
+
+    public ArrayList<String> getPValue(ArrayList<String> keys) {
+        Properties prop = new Properties();
+        InputStream is = null;
+        ArrayList<String> values = new ArrayList<>();
+        try {
+            is = new FileInputStream(configPath);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            prop.load(is);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        for (String key : keys) {
+            values.add(prop.getProperty(key));
+        }
+
+        return values;
+    }
+}
