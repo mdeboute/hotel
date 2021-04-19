629dc09d26b019c3f85343e6c6457634c4229031
DrMagazi <atakan.osman.coban@gmail.com>
2021-04-19 14:49:32 +0300

    feat(controllers/db): add method to get details of a specific booking

--- app/src/main/java/hotelproject/controllers/db/BookingsDB.java
+++ app/src/main/java/hotelproject/controllers/db/BookingsDB.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class BookingsDB {
     private final Connection conn;
@@ -24,5 +25,22 @@ public class BookingsDB {
             e.printStackTrace();
         }
         return bookings;
+    }
+
+    public Hashtable<String,String> getBookingDetails(int b_id) {
+        Hashtable<String, String> bookingDetails = new Hashtable<>();
+        String[] bookingHeaders = { "b_id", "r_num", "paid_by_card", "b_from", "b_till", "b_fee", "b_is_paid" };
+        try {
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM booking WHERE b_id = %d";
+            ResultSet rs = stmt.executeQuery(String.format(sql, b_id));
+            rs.next();
+            for (String header : bookingHeaders) {
+                bookingDetails.put(header, rs.getString(header));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return bookingDetails;
     }
 }
