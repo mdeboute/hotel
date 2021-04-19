eaa48c717c872c2f992f2f06a93ecc24c3035535
Oscar Johansson <ojoco09@student.lnu.se>
2021-04-19 08:00:24 +0200

    test(src.test.java.hotelproject.controllers.db): added test to databasemanagertest
    added tests to databasemanagertest

--- app/src/test/java/hotelproject/controllers/db/DatabaseManagerTest.java
+++ app/src/test/java/hotelproject/controllers/db/DatabaseManagerTest.java
@@ -2,6 +2,7 @@
 
 package hotelproject.controllers.db;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -15,16 +16,33 @@ public class DatabaseManagerTest {
     private DatabaseManager dbm;
     private ArrayList<String> log;
     private String tableName;
+    private String body;
 
     @Before
     public void setUp()  {
         dbm = new DatabaseManager();
         log = new ArrayList<>();
-        tableName = "room_type";
+        body = "test_column varchar(255)";
     }
 
     @Test
-    public void testIsTableExist() throws Exception {
+    public void testTableExist() throws Exception {
+        tableName = "room_type"; // Exists
         assertTrue(dbm.tableExists(tableName,log));
+    }
+
+    @Test
+    public void testCreateTable() {
+        tableName = "test_table";
+        dbm.createTable(tableName, body, log); // test
+        assertTrue(dbm.tableExists(tableName, log));
+    }
+
+    @Test
+    public void testDropTable() {
+        tableName = "test_table";
+        dbm.createTable(tableName, body, log);
+        dbm.dropTable(tableName, log); // test
+        assertFalse(dbm.tableExists(tableName, log));
     }
 }
