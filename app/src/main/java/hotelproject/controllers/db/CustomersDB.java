684d08e19b38a3399d6ca3b2d9c086366bb1f0d5
yd222br@student.lnu.se <yd222br@student.lnu.se>
2021-04-27 11:48:53 +0200

    feat(app.src.main.java.hotelproject.controllers.db) Add new methods for CustomersDB.

--- app/src/main/java/hotelproject/controllers/db/CustomersDB.java
+++ app/src/main/java/hotelproject/controllers/db/CustomersDB.java
@@ -3,80 +3,84 @@ package hotelproject.controllers.db;
 import hotelproject.controllers.objects.Customer;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CustomersDB {
 
     private final Connection conn;
-    public CustomersDB (Connection conn) {
+
+    public CustomersDB(Connection conn) {
         this.conn = conn;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /**
+     * @param customer The customer for checking.
+     * @return If customer's social security number already exists will return true, otherwise return false.
+     * @brief Identify if the customer already exists in the database.This method will be used by addCustomer() method.
+     */
+    public boolean customerExists(Customer customer) {
+        try {
+            String sql = "SELECT * FROM `customer`";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                if (rs.getInt("c_ss_number") == customer.getC_ss_number()) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
     /**
-     * @brief update customer in the database
-     * @param customer The updated Customer object
-     * @param oldCustomerSSNumber The old customer's social security number that is to be updated
+     * @param customer The inserted customer object.
+     * @brief Add a new customer if the social security number is not duplicated.
      */
-    public void updateCustomer(Customer customer, int oldCustomerSSNumber) {
-
+    public void addCustomer(Customer customer) {
         try {
-            String sql = "UPDATE `customer` SET `c_ss_number` = ?, `c_address` = ?, `c_full_name` = ?, `c_phone_num` = ?, `c_email` = ? WHERE `c_ss_number` = ? ";
+            if (customerExists(customer) == false) {
+                String sql = "INSERT INTO `customer` (`c_ss_number`, `c_address`, `c_full_name`, `c_phone_num`, `c_email`) VALUES (?,?,?,?,?)";
+                PreparedStatement statement = conn.prepareStatement(sql);
 
-            PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setInt(1, customer.getC_ss_number());
+                statement.setString(2, customer.getC_adress());
+                statement.setString(3, customer.getC_full_name());
+                statement.setInt(4, customer.getC_phone_num());
+                statement.setString(5, customer.getC_email());
 
-            statement.setInt(1,customer.getC_ss_number());
-            statement.setString(2,customer.getC_adress());
-            statement.setString(3,customer.getC_full_name());
-            statement.setInt(4,customer.getC_phone_num());
-            statement.setString(5,customer.getC_email());
-            statement.setInt(6,oldCustomerSSNumber);
-
-            statement.executeUpdate();
-
+                statement.executeUpdate();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-}
+        /**
+         * @brief update customer in the database.
+         * @param customer The updated Customer object.
+         * @param oldCustomerSSNumber The old customer's social security number that is to be updated.
+         */
+        public void updateCustomer (Customer customer,int oldCustomerSSNumber){
+
+            try {
+                String sql = "UPDATE `customer` SET `c_ss_number` = ?, `c_address` = ?, `c_full_name` = ?, `c_phone_num` = ?, `c_email` = ? WHERE `c_ss_number` = ? ";
+
+                PreparedStatement statement = conn.prepareStatement(sql);
+
+                statement.setInt(1, customer.getC_ss_number());
+                statement.setString(2, customer.getC_adress());
+                statement.setString(3, customer.getC_full_name());
+                statement.setInt(4, customer.getC_phone_num());
+                statement.setString(5, customer.getC_email());
+                statement.setInt(6, oldCustomerSSNumber);
+
+                statement.executeUpdate();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
