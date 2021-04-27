af5ef575a942886023dfd728d29fe69f246a9a44
yd222br@student.lnu.se <yd222br@student.lnu.se>
2021-04-27 09:33:54 +0200

    feat(app.src.main.java.hotelproject.controllers.db) Add connection and constructor.

--- app/src/main/java/hotelproject/controllers/db/CustomersDB.java
+++ app/src/main/java/hotelproject/controllers/db/CustomersDB.java
@@ -1,4 +1,13 @@
 package hotelproject.controllers.db;
 
+import java.sql.Connection;
+
 public class CustomersDB {
+
+    private final Connection conn;
+    public CustomersDB (Connection conn) {
+        this.conn = conn;
+    }
+
+
 }
