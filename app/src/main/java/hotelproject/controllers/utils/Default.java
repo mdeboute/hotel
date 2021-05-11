ab050fcfdbcf1660b43a054aa57233fd63c93ca5
DrMagazi <atakan.osman.coban@gmail.com>
2021-05-11 16:49:55 +0300

    feat(utils): add new default class for basic things like font

---
+++ app/src/main/java/hotelproject/controllers/utils/Default.java
@@ -0,0 +1,9 @@
+package hotelproject.controllers.utils;
+
+import javafx.scene.text.Font;
+
+public class Default {
+    public static Font getSFPro(int size) {
+        return Font.loadFont("file:assets/font/SF_Pro.ttf", size);
+    }
+}
