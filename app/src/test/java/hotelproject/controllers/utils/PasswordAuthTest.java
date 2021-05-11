99e5aa5b111b7ee7d27de53b748822dda2b12991
mdeboute <martin.deboute@gmail.com>
2021-05-11 15:09:22 +0200

    refactor(test) : tests are now ok

--- app/src/test/java/hotelproject/controllers/utils/PasswordAuthTest.java
+++ app/src/test/java/hotelproject/controllers/utils/PasswordAuthTest.java
@@ -1,25 +1,25 @@
 package hotelproject.controllers.utils;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class PasswordAuthTest {
 
-  private final PasswordAuth passwordAuth = new PasswordAuth();
-  private final String password = "password";
-  private final String token = passwordAuth.hash(password);
+    private final PasswordAuth passwordAuth = new PasswordAuth();
+    private final String password = "password";
+    private final String token = passwordAuth.hash(password);
 
 
-  @Test
-  public void testHash() {
-    String hashCode = passwordAuth.hash(password);
-    assertNotNull(hashCode);
-  }
+    @Test
+    public void testHash() {
+        String hashCode = passwordAuth.hash(password);
+        assertNotNull(hashCode);
+    }
 
-  @Test
-  public void testAuthenticate() {
-    assertTrue(passwordAuth.authenticate(password, token));
-  }
+    @Test
+    public void testAuthenticate() {
+        assertTrue(passwordAuth.authenticate(password, token));
+    }
 }
