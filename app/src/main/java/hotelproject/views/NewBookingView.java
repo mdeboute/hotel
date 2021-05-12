e17e00fd61cd42cdeb9ca62894409c4c78a122b7
Oscar Johansson <ojoco09@student.lnu.se>
2021-05-12 21:09:45 +0200

    fix(src.main.java.hotelproject.views): fix bugs in bookingsview and newbookingsview
    fix bugs in bookingsview and newbookingsview

--- app/src/main/java/hotelproject/views/NewBookingView.java
+++ app/src/main/java/hotelproject/views/NewBookingView.java
@@ -90,10 +90,10 @@ public class NewBookingView extends View {
         checkOut.setPromptText("DD/MM/YYYY");
 
         Label bookingIDL = new Label("Booking number: ");
-        pane.add(bookingIDL, 0, 1);
+        //pane.add(bookingIDL, 0, 1);
         //pane.add(bookingIDL, 1, 1);
         Label numRoomL = changeLabelDesign(new Label("Room number: "), "file:assets/font/SF_Pro.ttf", 20, "white");
-        //pane.add(numRoomL, 0, 1);
+        pane.add(numRoomL, 0, 1);
         pane.add(numRoom, 1, 1);
         pane.add(checkIn, 0, 2);
         pane.add(checkOut, 1, 2);
