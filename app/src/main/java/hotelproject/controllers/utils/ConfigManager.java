2d2caefc26ae88ddb5ea90b8c8e359faad4e2469
yd222br@student.lnu.se <yd222br@student.lnu.se>
2021-05-19 21:20:21 +0200

    docs(app.src.main.java.hotelproject.controllers.utils) add javadoc to ConfigManager.java

--- app/src/main/java/hotelproject/controllers/utils/ConfigManager.java
+++ app/src/main/java/hotelproject/controllers/utils/ConfigManager.java
@@ -19,6 +19,12 @@ public class ConfigManager {
         this.configPath = configPath;
     }
 
+    /**
+     *
+     * @param key for getting the focal property's value.
+     * @return the property return as a string.
+     * @brief According a file path to get configuration property.
+     */
     public String getPValue(String key) {
         Properties prop = new Properties();
         InputStream is = null;
@@ -36,6 +42,11 @@ public class ConfigManager {
         return prop.getProperty(key);
     }
 
+    /**
+     * @param keys All the keys for getting the values.
+     * @return All the values will saved in ArrayList.
+     * @brief According several file paths to get several configuration properties.
+     */
     public ArrayList<String> getPValue(ArrayList<String> keys) {
         Properties prop = new Properties();
         InputStream is = null;
