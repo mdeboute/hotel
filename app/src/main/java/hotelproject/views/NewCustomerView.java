3d747acad976f89a493f8fe219e800555c58f5ee
Fabian Dacic <fd222fr@student.lnu.se>
2021-05-27 00:22:16 +0200

    refactor(app.src.main.java.hotelproject.views) New customer textfield restraints

--- app/src/main/java/hotelproject/views/NewCustomerView.java
+++ app/src/main/java/hotelproject/views/NewCustomerView.java
@@ -1,11 +1,15 @@
 package hotelproject.views;
 
+import java.util.regex.Pattern;
+
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -43,17 +47,62 @@ public class NewCustomerView extends View {
         Label cSSNumL = changeLabelDesign(new Label("Social security number: "), "file:assets/font/SF_Pro.ttf", 17, "white");
         pane.add(cSSNumL, 0, 1);
         pane.add(cSSNum, 1, 1);
+
+        // force the field to be numeric only
+        cSSNum.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("[1-9]|[1-9][0-9]{1,7}")) {
+                Platform.runLater(cSSNum::clear);
+            }
+
+            // If there numbers and no violations (characters, signs or such), limit to 8
+            // only
+            Pattern pattern = Pattern.compile(".{0,8}");
+            TextFormatter<String> formatter = new TextFormatter<>(
+                    change -> pattern.matcher(change.getControlNewText()).matches() ? change : null);
+            cSSNum.setTextFormatter(formatter);
+        });
         cSSNum.setPromptText("8 digits");
+
         Label cAddressL = changeLabelDesign(new Label("Address: "), "file:assets/font/SF_Pro.ttf", 17, "white");
         pane.add(cAddressL, 0, 2);
         pane.add(cAddress, 1, 2);
+
+        // force the field to be strings only
+        cAddress.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$")) {
+                Platform.runLater(cAddress::clear);
+            }
+        });
+
         Label cFullNameL = changeLabelDesign(new Label("Name: "), "file:assets/font/SF_Pro.ttf", 17, "white");
         pane.add(cFullNameL, 0, 3);
         pane.add(cFullName, 1, 3);
+
+        // force the field to be numeric only
+        cFullName.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("^[\\p{L} .'-]+$")) {
+                Platform.runLater(cFullName::clear);
+            }
+        });
+
         Label cPhoneNumL = changeLabelDesign(new Label("Phone number: "), "file:assets/font/SF_Pro.ttf", 17, "white");
         pane.add(cPhoneNumL, 0, 4);
         pane.add(cPhoneNum, 1, 4);
         cPhoneNum.setPromptText("9 digits");
+
+        // force the field to be numeric only
+        cPhoneNum.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("[1-9]|[1-9][0-9]{1,8}")) {
+                Platform.runLater(cPhoneNum::clear);
+            }
+
+            // If there numbers and no violations (characters, signs or such), limit to 8
+            Pattern pattern = Pattern.compile(".{0,9}");
+            TextFormatter<String> formatter = new TextFormatter<>(
+                    change -> pattern.matcher(change.getControlNewText()).matches() ? change : null);
+            cPhoneNum.setTextFormatter(formatter);
+        });
+
         Label cEmailL = changeLabelDesign(new Label("Email: "), "file:assets/font/SF_Pro.ttf", 17, "white");
         pane.add(cEmailL, 0, 5);
         pane.add(cEmail, 1, 5);
